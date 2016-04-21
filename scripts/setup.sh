03b7efe0b380547c06a4af91e03eebce492d33f3
Nick Kuhn <nicklz22@yahoo.com>
2016-04-20 18:29:43 -0700

    Adding symlinks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,19 +10,18 @@ cd ~/www/ && git config --global user.email "nicholas.kuhn@spi.com"
 cd ~/www/ && git config --global user.name "Nick Kuhn"
 cd ~/www/ && git config --global core.editor "vim"
 
-#set up repos
+#set up git repos
 cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-interface.git
 cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-server.git
 cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-learning-module.git
 cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-newplayer.git
 
+#ruby - vagrant makes this hard but this should sort things out
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-
 source /home/vagrant/.rvm/scripts/rvm
 rvm install ruby-1.9.3-p551 
 rvm use ruby-1.9.3-p551 
-
 
 #nodejs
 curl -sL https://deb.nodesource.com/setup | sudo bash - && yes | sudo apt-get install nodejs
@@ -42,16 +41,31 @@ mkdir /home/vagrant/mysql_backups
 rm -rf ~/www/elx-learning-module/sites/drupal && cp -rf ~/www/drupal ~/www/elx-learning-module/sites/
 cd ~/www/elx-server/packages/custom/elx/tools/ && ./vagrant-sync-mysql.sh
 
-
 #npn stuff
 yes |  sudo npm install -g bower
-sudo chown vagrant:vagrant /home/vagrant/.config/ -R
-cd ~/www/elx-server && bower update
 yes |  sudo npm install -g gulp
 yes |  sudo npm install -g grunt
 yes |  sudo npm install -g time-grunt
 
+#build / update the codebases
 sudo chown vagrant:vagrant /home/vagrant/.config/ -R
-cd ~/www/elx-interface && bower update
+cd ~/www/elx-server && bower update && ./jenkins.sh
 
-cd ~/www/elx-newplayer && bower update
+sudo chown vagrant:vagrant /home/vagrant/.config/ -R
+cd ~/www/elx-interface && bower update && ./jenkins.sh
+
+sudo chown vagrant:vagrant /home/vagrant/.config/ -R
+cd ~/www/elx-newplayer && bower update && ./jenkins.sh
+
+#wire everything up
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/app /home/vagrant/www/elx-interface/src/app
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/bower_components /home/vagrant/www/elx-interface/bower_components
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/fonts /home/vagrant/www/elx-interface/dist/fonts
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/inject /home/vagrant/www/elx-interface/dist/inject
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/locales /home/vagrant/www/elx-interface/locales
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/scripts /home/vagrant/www/elx-interface/dist/scripts
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/styles /home/vagrant/www/elx-interface/dist/styles
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/fonts/ /home/vagrant/www/elx-interface/src/app/theme/fonts/
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/styles/theme/fonts /home/vagrant/www/elx-interface/src/app/theme/fonts
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/public/assets/styles/theme/images /home/vagrant/www/elx-interface/src/app/theme/images
+ln -s /home/vagrant/www/elx-server/packages/custom/elx/server/views/index.html /home/vagrant/www/elx-interface/dist/index.html
