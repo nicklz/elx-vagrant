e598b736160bbe3e8e2eafba448c32c7dad3a1f3
Nick Kuhn <nicklz22@yahoo.com>
2016-04-26 00:30:05 -0700

    Updates

--- scripts/finish.sh
+++ scripts/finish.sh
@@ -5,57 +5,9 @@ sudo locale-gen  en_US.UTF-8
 export LC_ALL="en_US.UTF-8" 
 export LANG="en_US.UTF-8" 
 
-#TODO: this needs to be added to a config file
-cd ~/www/ && git config --global user.email "nicholas.kuhn@spi.com"
-cd ~/www/ && git config --global user.name "Nick Kuhn"
-cd ~/www/ && git config --global core.editor "vim"
-
-#set up git repos
-
-rm -rf /home/vagrant/www/elx-interface
-rm -rf /home/vagrant/www/elx-server
-rm -rf /home/vagrant/www/elx-learning-module
-rm -rf /home/vagrant/www/elx-newplayer
-
-cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-interface.git
-cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-server.git
-cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-learning-module.git
-cd ~/www/ && git clone git@github.com:MasterDoublePrime/elx-newplayer.git
-
-#ruby - vagrant makes this hard but this should sort things out
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source /home/vagrant/.rvm/scripts/rvm
-rvm install ruby-1.9.3-p551 
-rvm use ruby-1.9.3-p551 
-
-#nodejs
-curl -sL https://deb.nodesource.com/setup | sudo bash - && yes | sudo apt-get install nodejs
-
-#mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-sudo apt-get update
-yes |  sudo apt-get install -y mongodb-org
-sudo mkdir -p /data/db
-sudo mongod --port 27020 &
-mkdir /home/vagrant/mongo_backups
-cd ~/www/elx-server/packages/custom/elx/tools/ && ./vagrant-sync-mongo.sh
-
-#mysql
-mkdir /home/vagrant/mysql_backups
-rm -rf ~/www/elx-learning-module/sites/drupal && cp -rf ~/www/drupal ~/www/elx-learning-module/sites/
-cd ~/www/elx-server/packages/custom/elx/tools/ && ./vagrant-sync-mysql.sh
-
-#npn stuff
-sudo npm update npm -g
-
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-nvm install v
-nvm alias default v5.11.0
-
-
 yes |  sudo npm install -g bower gulp  grunt time-grunt karma  forever node-gyp  mean-cli
+
+
 
 
 #build / update the codebases
@@ -70,6 +22,7 @@ cd ~/www/elx-interface && bower update &&  sudo  ./jenkins.sh
 
 #wire everything up
 mkdir /home/vagrant/www/elx-server/packages/custom/elx/public/assets
+
 ln -s /home/vagrant/www/elx-interface/src/app /home/vagrant/www/elx-server/packages/custom/elx/public/assets/app
 ln -s /home/vagrant/www/elx-interface/bower_components /home/vagrant/www/elx-server/packages/custom/elx/public/assets/bower_components 
 ln -s /home/vagrant/www/elx-interface/src/fonts /home/vagrant/www/elx-server/packages/custom/elx/public/assets/fonts 
