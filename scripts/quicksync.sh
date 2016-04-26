63ffe0975aa98bb63e2a89ce3ba820b56a021630
Nick Kuhn <nicklz22@yahoo.com>
2016-04-26 00:19:58 -0700

    P More scripts

--- scripts/quicksync.sh
+++ scripts/quicksync.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 
+
 mongo mean-dev --eval "printjson(db.dropDatabase())"
 mongo mean-prod --eval "printjson(db.dropDatabase())"
 mongo mean-backup --eval "printjson(db.dropDatabase())"
 
-cd /home/vagrant/mongo_backups/ && mongorestore mean-prod-current
+cd ~/mongo_backups/ && mongorestore mean-prod-current
+
+
+
+cd ~/www/elx-learning-module/sites/drupal && drush sql-cli < ~/mysql_backups/current/current.sql
+
 
 
 
